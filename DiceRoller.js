d63c56bd8d082a20aa8f9c3847877f36c3ca807d
Singireddy Sravani Tulasi <147299313+sravanis04@users.noreply.github.com>
2025-02-17 20:22:51 +0530

    Add files via upload

---
+++ DiceRoller.js
@@ -0,0 +1,19 @@
+// Dice Roller
+
+
+function rollDice(){
+
+    const noOfDice=document.getElementById("noOfDice").value;
+    const diceResult=document.getElementById("diceResult");
+    const diceImages=document.getElementById("diceImages");
+    const values=[];
+    const images=[];
+
+    for(let i=0 ; i<noOfDice ; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="dice_images/${value}.png">`);
+    }
+    diceResult.textContent=`dice :${values.join(', ')}`;
+    diceImages.innerHTML=images.join('');
+} 
